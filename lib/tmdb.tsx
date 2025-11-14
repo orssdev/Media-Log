e5428d28cf0f8512ed009915049f8d16d872decb
Oscar <orss.dev@gmail.com>
2025-11-13 23:29:46 -0500

    tv show update

--- lib/tmdb.tsx
+++ lib/tmdb.tsx
@@ -1,5 +1,5 @@
 export async function searchMovies(query: string) {
-  const res = await fetch(`/api/moviesearch?q=${encodeURIComponent(query)}`);
+  const res = await fetch(`/api/movie/search?q=${encodeURIComponent(query)}`);
 
   if (!res.ok) throw new Error('Failed search movies');
 
@@ -8,9 +8,27 @@ export async function searchMovies(query: string) {
 }
 
 export async function getPopularMovies() {
-  const res = await fetch(`/api/popular`);
+  const res = await fetch(`/api/movie/popular`);
 
   if (!res.ok) throw new Error('Failed to fetch popular movies');
+
+  const data = await res.json();
+  return data.results;
+}
+
+export async function searchTvShows(query: string) {
+  const res = await fetch(`/api/tv/search?q=${encodeURIComponent(query)}`);
+
+  if (!res.ok) throw new Error('Failed to search TV shows');
+
+  const data = await res.json();
+  return data.results;
+}
+
+export async function getPopularShows() {
+  const res = await fetch(`/api/tv/popular`);
+
+  if (!res.ok) throw new Error('Failed to fetch popular shows');
 
   const data = await res.json();
   return data.results;
