1e88092142e6abd09c538d18250d0e0696ee0e83
Oscar <orss.dev@gmail.com>
2025-11-14 21:30:40 -0500

    center-div

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="page">
       <div className="flex items-center flex-col">
-        <div className="w-full flex flex-wrap justify-center items-center gap-5">
+        <div className="w-full center-div flex-wrap gap-5">
           <MediaCounter media={'Movies'} count={372} route="/movie"/>
           <MediaCounter media={'Games'} count={294} route="/game"/>
           <MediaCounter media={'TV Shows'} count={420} route="/tv"/>
