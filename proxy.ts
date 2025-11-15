f3baf7b468510998c16304b4bdb5af5c15afbe69
Oscar <orss.dev@gmail.com>
2025-11-15 03:22:40 -0500

    exporting authMiddleware

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,6 @@
-import { withAuth } from "next-auth/middleware";
+import authMiddleware from "next-auth/middleware";
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token,
-  },
-  pages: {
-    signIn: "/auth/signin",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+export default authMiddleware;
 
 export const config = {
   matcher: ["/movie/add", "/tv/add", "/game/add"],
